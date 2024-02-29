9adf20e4e30b95ad1667a0e99cb4c197e16357cc
ngozika89 <ngozi.ubadigha@students.plymouth.ac.uk>
2024-02-29 11:46:38 +0000

    done

--- Tasks/HelloWorld/main.cpp
+++ Tasks/HelloWorld/main.cpp
@@ -7,7 +7,7 @@ int main()
 
     int a;
 
-    for (a=0; a < 5; a = a + 1) 
+    for (a=0; a < 4; a = a + 1) 
     {
         printf("a = %d\n",a);
     }
@@ -16,4 +16,5 @@ int main()
 
     while (true);
 }
+
 
